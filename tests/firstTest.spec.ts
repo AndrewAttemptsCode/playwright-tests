7d0ea2ece72bd202cb2d45ff976af732e17a7116
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-03-21 17:28:54 +0000

    feat: add navigation to datepicker page and beforeEach hook for DRY code

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,8 +1,16 @@
 import { test, expect } from "@playwright/test";
 
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://localhost:4200/');
+  await page.getByRole("link", { name: "Forms" }).click();
+});
+
 test("Navigage to forms page", async ({ page }) => {
-  await page.goto("http://localhost:4200/");
-  await page.getByRole('link', { name: 'Forms' }).click();
-  await page.getByRole('link', { name: 'Form Layouts' }).click();
-  await expect(page.getByText('Inline form')).toBeVisible();
+  await page.getByRole("link", { name: "Form Layouts" }).click();
+  await expect(page.getByText("Inline form")).toBeVisible();
+});
+
+test("Navigage to datepicker page", async ({ page }) => {
+  await page.getByRole("link", { name: "Datepicker" }).click();
+  await expect(page.getByText("Common Datepicker")).toBeVisible();
 });
