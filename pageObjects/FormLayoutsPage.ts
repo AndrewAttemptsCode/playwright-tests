ca901e7c36a4610fad633eaa2f79a7fab4b95c9c
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-04-02 20:03:58 +0100

    feat: add singing into inline form method

--- pageObjects/FormLayoutsPage.ts
+++ pageObjects/FormLayoutsPage.ts
@@ -1,4 +1,4 @@
-import { type Page } from "@playwright/test";
+import { expect, type Page } from "@playwright/test";
 
 class FormLayoutsPage {
   private readonly page: Page;
@@ -13,6 +13,25 @@ class FormLayoutsPage {
     await form.getByLabel(/password/i).fill(password);
     await form.getByRole("radio", { name: radioOption }).check({ force: true });
     await form.getByRole("button", { name: /sign in/i }).click();
+  }
+
+  async signinInline(name: string, email: string, rememberMe: boolean) {
+    const form = this.page.locator("nb-card", { hasText: /inline form/i });
+    await form.getByPlaceholder(/jane doe/i).fill(name);
+    await form.getByPlaceholder(/email/i).fill(email);
+    
+    const checkbox = form.getByRole("checkbox");
+    const isChecked = await checkbox.isChecked();
+
+    if (rememberMe !== isChecked) {
+      if (rememberMe) {
+        await checkbox.check({ force: true });
+      } else {
+        await checkbox.uncheck({ force: true });
+      }
+    }
+
+    await form.getByRole("button", { name: /submit/i }).click();
   }
 
 }
