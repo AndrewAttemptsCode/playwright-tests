2ce976d6b94e7a40cc0f7598f17981f57c45636e
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-04-18 20:42:29 +0100

    feat: update to include test tags to describe block of tests and split up tests

--- tests/datepicker.spec.ts
+++ tests/datepicker.spec.ts
@@ -5,20 +5,28 @@ test.beforeEach(async ({ page }) => {
   await page.goto("/");
 });
 
-test.describe("Date picker tests", () => {
-  test("Single date, n days from today", async ({ page }) => {
+test.describe("Date picker tests @regression @ui", () => {
+  test("select 10 days from today", async ({ page }) => {
     const pm = new PageManager(page);
     await pm.navigateTo().datepickerPage();
-
     await pm.onDatePickerPage().daysFromToday(10);
+  });
+
+  test("select 30 days from today", async ({ page }) => {
+    const pm = new PageManager(page);
+    await pm.navigateTo().datepickerPage();
     await pm.onDatePickerPage().daysFromToday(30);
   });
 
-  test("Range dates, n days from today", async ({ page })=> {
+  test("select range 2 days to 5 days from today", async ({ page })=> {
     const pm = new PageManager(page);
     await pm.navigateTo().datepickerPage();
-
     await pm.onDatePickerPage().rangeFromToday(2, 5);
+  });
+
+  test("select range 5 days to 62 days from today", async ({ page })=> {
+    const pm = new PageManager(page);
+    await pm.navigateTo().datepickerPage();
     await pm.onDatePickerPage().rangeFromToday(5, 62);
   });
 
